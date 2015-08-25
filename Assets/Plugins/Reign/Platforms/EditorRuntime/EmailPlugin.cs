1e29995f39dc65dfeda9c94bc6a8d13d3ad667a4
Andrew Witte <zezba9000@gmail.com>
2015-08-25 10:50:13 -0700

    Fixed email invoke bugs.

--- Assets/Plugins/Reign/Platforms/EditorRuntime/EmailPlugin.cs
+++ Assets/Plugins/Reign/Platforms/EditorRuntime/EmailPlugin.cs
@@ -11,11 +11,23 @@ namespace Reign.Plugin
 		{
 			UnityEngine.Debug.Log(string.Format("Send mail: To={0} Subject={1} Body={2}", to, subject, body));
 
+			char delimiter = '?';
 			var theStringBuilder = new StringBuilder();
-			theStringBuilder.Append("mailto:" + to + "?");
-			theStringBuilder.Append("&subject=" + subject);
-			theStringBuilder.Append("&body=" + body);
+			theStringBuilder.Append("mailto:" + to);
+			if (!string.IsNullOrEmpty(subject))
+			{
+				theStringBuilder.Append(delimiter + "subject=" + subject);
+				delimiter = '&';
+			}
+
+			if (!string.IsNullOrEmpty(body))
+			{
+				theStringBuilder.Append(delimiter + "body=" + body);
+				delimiter = '&';
+			}
+
 			//&attachment="/files/audio/attachment.mp3"
+
 			#if UNITY_WEBPLAYER
 			UnityEngine.Application.OpenURL(theStringBuilder.ToString());
 			#else
